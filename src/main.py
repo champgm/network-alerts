90fa5a52b781012a2e01d0205e4c183cca660d22
champgm <2091382+champgm@users.noreply.github.com>
2024-02-22 09:55:30 -0500

    A little cleanup

--- src/main.py
+++ src/main.py
@@ -5,10 +5,34 @@ from send_mail import send_email
 import time
 
 
-def is_ip_reachable(ip):
+def ip_is_reachable(ip):
     # Timeout is in seconds
     response = ping(ip, timeout=1, count=5)
     return response.success()
+
+
+def send_warning(config: Config):
+    print(f"[{now()}] Will send WARNING email to {len(config.email.recipients)} recipients.")
+    for recipient in config.email.recipients:
+        send_email(
+            config.email.sender,
+            recipient,
+            "Freezer plug is offline!",
+            "Go check it!",
+            config,
+        )
+
+
+def send_all_clear(config: Config):
+    print(f"[{now()}] Will send ALL CLEAR email to {len(config.email.recipients)} recipients.")
+    for recipient in config.email.recipients:
+        send_email(
+            config.email.sender,
+            recipient,
+            "Freezer plug has come back online.",
+            "Phew.",
+            config,
+        )
 
 
 def main():
@@ -16,33 +40,20 @@ def main():
     failures = 0
     warning_email_sent = False
     while True:
-        if not is_ip_reachable(config.monitoring.target_ip):
-            failures += 1
+        if not ip_is_reachable(config.monitoring.target_ip):
             print(f"[{now()}] IP {config.monitoring.target_ip} is not reachable.")
+            failures += 1
             if failures > config.monitoring.failures_allowed:
-                print(f"[{now()}] Will send email to {len(config.email.recipients)} recipients.")
-                for recipient in config.email.recipients:
-                    send_email(
-                        config.email.sender,
-                        recipient,
-                        "Freezer plug is offline!",
-                        "Go check it!",
-                        config,
-                    )
-                    warning_email_sent = True
-                    failures = 0
+                send_warning(config)
+                warning_email_sent = True
+                failures = 0
         else:
-            failures = 0
             print(f"[{now()}] IP {config.monitoring.target_ip} is reachable.")
+            failures = 0
             if warning_email_sent:
-                for recipient in config.email.recipients:
-                    send_email(
-                        config.email.sender,
-                        recipient,
-                        "Freezer plug has come back online.",
-                        "Phew.",
-                    )
-                    warning_email_sent = False
+                send_all_clear(config)
+                warning_email_sent = False
+
         if failures == 0:
             print(f"[{now()}] Sleeping for {config.monitoring.check_interval}s.")
             time.sleep(config.monitoring.check_interval)
--- src/send_mail.py
+++ src/send_mail.py
@@ -5,25 +5,29 @@ import urllib.parse
 from configuration import Config
 
 tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token"
+access_token_headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
 
 def get_access_token(config: Config) -> str:
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
     config_json = config.email.google_token.model_dump()
-    print(f"Access token request JSON:{config_json}")
     body = urllib.parse.urlencode(config_json)
-    response = requests.post(tokenEndpoint, headers=headers, data=body)
-    response.raise_for_status()  # Raises if the response code is 4XX/5XX
+
+    response = requests.post(tokenEndpoint, headers=access_token_headers, data=body)
+    response.raise_for_status()
+
     return response.json().get("access_token")
 
 
 def format_email(sender: str, recipient: str, subject: str, body: str) -> str:
-    return f"To: {recipient}\nFrom: {sender}\nSubject: {handle_unicode(subject)}\n\n{body if body else '.'}\n"
+    encoded_subject = handle_unicode(subject)
+    nonempty_body = body if body else "."
+    return f"To: {recipient}\nFrom: {sender}\nSubject: {encoded_subject}\n\n{nonempty_body}\n"
 
 
 def handle_unicode(string_value: str) -> str:
+    """
+    This function base-64 encodes the subject, which allows unicode characters to be included.
+    """
     encoded = base64.b64encode(string_value.encode()).decode()
     return f"=?utf-8?B?{encoded}?="
 
@@ -31,20 +35,19 @@ def handle_unicode(string_value: str) -> str:
 def send_email(
     sender: str, recipient: str, subject: str, body: str, config: Config
 ) -> Dict[str, Any]:
+    gmail_endpoint = f"https://www.googleapis.com/upload/gmail/v1/users/{sender}/messages/send"
+
+    # The request needs the appropriate auth header
     bearer_token = get_access_token(config)
     authorization_header = f"Bearer {bearer_token}"
-    headers = {
-        "content-type": "message/rfc822",
-        "authorization": authorization_header,
-    }
+    headers = {"content-type": "message/rfc822", "authorization": authorization_header}
+
+    # emails must be formatted in a specific way
     email_content = format_email(sender, recipient, subject, body)
-    gmail_endpoint = f"https://www.googleapis.com/upload/gmail/v1/users/{sender}/messages/send"
-    response = requests.post(
-        gmail_endpoint,
-        headers=headers,
-        data=email_content,
-    )
+
     print(f"Sending email with parameters: headers={headers}, body={email_content}")
+    response = requests.post(gmail_endpoint, headers=headers, data=email_content)
     response.raise_for_status()
+
     print(f"Send email result: {response.status_code}, {response.text}")
     return response.json()
